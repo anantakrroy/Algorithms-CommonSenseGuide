278125d69dff8980d07316de5e8a5d03245ae88a
anantakrroy <kr.innovate@gmail.com>
2025-02-27 10:06:09 +0530

    refactor: count number of X's in string base case improve

--- recursion/countX.js
+++ recursion/countX.js
@@ -1,13 +1,17 @@
 // count the number of X's in the string
 function countX(str) {
-    console.log(`calling with str = ${str}`)
+    // console.log(`calling with str = ${str}`)
     // base case - when str is length 1 
-    if(str.length == 1) {
-        if(str[0] == 'x')
-            return 1
-        else
-            return 0
-    }
+    // if(str.length == 1) {
+    //     if(str[0] == 'x')
+    //         return 1
+    //     else
+    //         return 0
+    // }
+
+    // improved base case
+    if(str.length === 0)
+        return 0
 
     // subproblem - call count on rest of string except 1st char, add 1 to count if 1st char is 'x'
     if(str[0] == 'x') {
@@ -17,4 +21,4 @@ function countX(str) {
     }
 }
 
-console.log(countX('abxcxdxw'))
+console.log(countX('dssjksjskdj'))
